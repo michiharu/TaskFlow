a5f64987647600fccba46e0a3c178da844997f52
michiharu <michi1diver@gmail.com>
2021-10-04 01:45:41 +0900

    :sparkles: create uuid function

---
+++ src/funcs/utils.test.ts
@@ -0,0 +1,21 @@
+import { uuid4 } from './utils';
+
+describe('uuid4', () => {
+  const count = 100_000;
+  const ids: Set<string> = new Set();
+  for (let i = 0; i < count; i++) ids.add(uuid4());
+
+  test('id should be unique.', () => {
+    expect(ids.size).toBe(count);
+  });
+
+  test('id has a formal defined format.', () => {
+    //          xxxxxxxx   - xxxx      -4xxx        -yxxx             -xxxxxxxxxxxx
+    const re = /[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/;
+    expect(re.test('12345678-9abc-4def-9000-000000000000')).toBe(true);
+    expect(re.test('12345678-9abc-0def-9000-000000000000')).toBe(false);
+    expect(re.test('12345678-9abc-4def-0000-000000000000')).toBe(false);
+    expect(re.test('12345678-9abc-4def-f000-000000000000')).toBe(false);
+    for (const id in ids) expect(re.test(id)).toBe(true);
+  });
+});
