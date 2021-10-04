96d0d1be0ba63b61443540e59292e869820963f0
michiharu <michi1diver@gmail.com>
2021-10-05 01:11:58 +0900

    :+1; toolbar for tree

---
+++ src/components/TreeToolbar.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+
+const TreeToolbar: React.FC = () => {
+  return (
+    <Toolbar>
+      <Button>save</Button>
+    </Toolbar>
+  );
+};
+
+export default TreeToolbar;
