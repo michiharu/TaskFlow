500ff69557d0d0a0cd3e32c671d8e3286366a1fd
michiharu <michi1diver@gmail.com>
2021-10-09 14:14:45 +0900

    :sparkles: create components, FlowRenderer FlowCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 
-import { Layer, Rect } from 'react-konva';
-
 import { Box, Container } from '@mui/material';
 
-import FlexibleStage from './components/FlexibleStage';
+import FlowRenderer from './components/FlowRenderer';
 import { containerMaxWidth } from './const';
 import AppHeader from './layouts/AppHeader';
 
@@ -14,11 +12,7 @@ const App: React.FC = () => {
       <AppHeader />
       <Container component="main" maxWidth={containerMaxWidth}>
         <Box sx={{ height: 'calc(100vh - 64px)', p: 1 }}>
-          <FlexibleStage stageSize={{ width: 5000, height: 5000 }}>
-            <Layer>
-              <Rect width={100} height={20} fill="#fff" />
-            </Layer>
-          </FlexibleStage>
+          <FlowRenderer />
         </Box>
       </Container>
     </>
---
+++ src/Providers.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+
+import { CssBaseline } from '@mui/material';
+
+import App from './App';
+import { store } from './store/setup-store';
+import theme from './theme';
+
+const Providers: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  );
+};
+
+export default Providers;
