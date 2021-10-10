22ce049640f87f1a1d58aa87d71852ed5c312825
michiharu <michi1diver@gmail.com>
2021-10-10 17:11:45 +0900

    :+1: add buttons to card

--- src/components/FlowRenderer.tsx
+++ src/components/FlowRenderer.tsx
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 import { RootState } from '../store/setup-store';
 import { treeSelectors } from '../store/tree/tree-slice';
 import { Size } from '../types';
-import { FlowEntity } from '../types/tree-node';
+import { FlowEntity, TreeSettings } from '../types/tree-node';
 
 import FlexibleStage from './FlexibleStage';
 import FlowCard from './FlowCard';
@@ -14,18 +14,19 @@ import FlowCard from './FlowCard';
 type StateProps = {
   rootEntity: FlowEntity;
   entities: FlowEntity[];
-  stagePadding: number;
+  settings: TreeSettings;
 };
 
-const FlowRenderer: React.FC<StateProps> = ({ rootEntity, entities, stagePadding }) => {
-  const { size } = rootEntity;
-  if (!size) throw new Error();
-  const stageSize: Size = { width: size.tree.width + stagePadding * 2, height: size.tree.height + stagePadding * 2 };
+const FlowRenderer: React.FC<StateProps> = ({ rootEntity, entities, settings }) => {
+  const { tree } = rootEntity;
+  if (!tree) throw new Error();
+  const { stagePadding } = settings;
+  const stageSize: Size = { width: tree.width + stagePadding * 2, height: tree.height + stagePadding * 2 };
   return (
     <FlexibleStage stageSize={stageSize}>
       <Layer>
         {entities.map((entity) => (
-          <FlowCard key={entity.id} entity={entity} />
+          <FlowCard key={entity.id} entity={entity} settings={settings} />
         ))}
       </Layer>
     </FlexibleStage>
@@ -35,12 +36,10 @@ const FlowRenderer: React.FC<StateProps> = ({ rootEntity, entities, stagePadding
 const mapStateToProps = (state: RootState): StateProps => {
   const { entities, rootId, settings } = state.tree;
   if (!rootId) throw new Error();
-
-  const { stagePadding } = settings;
   return {
     rootEntity: entities[rootId] as FlowEntity,
     entities: treeSelectors.selectAll(state),
-    stagePadding,
+    settings,
   };
 };
 
