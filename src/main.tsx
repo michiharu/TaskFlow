d1a204febc7bae7a5f4db0d8c964329cfc1f3057
michiharu <michi1diver@gmail.com>
2021-10-18 13:46:00 +0900

    :+1: setup connected-react-router

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,24 @@ import * as React from 'react';
 import { render } from 'react-dom';
 
 import { ThemeProvider } from '@mui/material/styles';
+import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
 import { CssBaseline } from '@mui/material';
 
 import App from './App';
-import { store } from './store/setup-store';
+import { history, store } from './store/setup-store';
 import { theme } from './theme';
 
 const Providers: React.FC = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <ConnectedRouter history={history}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <App />
         </ThemeProvider>
-      </BrowserRouter>
+      </ConnectedRouter>
     </Provider>
   );
 };
--- src/store/setup-store.ts
+++ src/store/setup-store.ts
@@ -1,11 +1,16 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
 import createSagaMiddleware from 'redux-saga';
 
 import { entitySlice } from './flow-entity/slice';
 import { flowSlice } from './flow/slice';
 import { rootSaga } from './root-saga';
 
-export const rootReducer = combineReducers({
+export const history = createBrowserHistory();
+
+const rootReducer = combineReducers({
+  router: connectRouter(history),
   flow: flowSlice.reducer,
   entity: entitySlice.reducer,
 });
@@ -14,10 +19,11 @@ export type RootState = ReturnType<typeof rootReducer>;
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const setupStore = () => {
+  const router = routerMiddleware(history);
   const saga = createSagaMiddleware();
   const store = configureStore({
     reducer: rootReducer,
-    middleware: (getDefault) => getDefault({ thunk: false }).concat(saga),
+    middleware: (getDefault) => getDefault({ thunk: false }).concat([router, saga]),
   });
   saga.run(rootSaga);
   return store;
