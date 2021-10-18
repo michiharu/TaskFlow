a0210ffbc061ac0e0319e2f362682697c03c6eeb
michiharu <michi1diver@gmail.com>
2021-10-18 13:46:49 +0900

    :sparkles: use connected-react-router

--- src/components/FlowList.tsx
+++ src/components/FlowList.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 
-import { connect } from 'react-redux';
+import { push } from 'connected-react-router';
+import { connect, useDispatch } from 'react-redux';
 
 import { Card, CardActionArea, CardContent, Grid, Typography } from '@mui/material';
 
+import { entitySlice } from '../store/flow-entity/slice';
 import { flowSelectors } from '../store/flow/slice';
 import { RootState } from '../store/setup-store';
 import { Flow } from '../types/flow';
@@ -13,12 +15,19 @@ type StateProps = {
 };
 
 const FlowList: React.FC<StateProps> = ({ flows }) => {
+  const dispatch = useDispatch();
+
+  const handleClickCard = (flow: Flow) => () => {
+    dispatch(entitySlice.actions.setFlow(flow));
+    dispatch(push(`/flow/${flow.id}`));
+  };
+
   return (
     <Grid container sx={{ mt: 2 }} spacing={2}>
       {flows.map((flow) => (
         <Grid key={flow.id} item xs={6} md={4} lg={3}>
           <Card sx={{ minWidth: 200 }}>
-            <CardActionArea>
+            <CardActionArea onClick={handleClickCard(flow)}>
               <CardContent sx={{ height: 200 }}>
                 <Typography>{flow.title}</Typography>
               </CardContent>
