43d87fbb01910256e4178d7b5eb103d186dd8b7d
michiharu <michi1diver@gmail.com>
2021-10-30 19:30:19 +0900

    :+1: remove width limit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import * as React from 'react';
 
-import { Box, Container } from '@mui/material';
-
 import PageRouter from './components/PageRouter';
-import { containerMaxWidth } from './const';
 import AppBar from './layouts/AppBar';
 
 const App: React.FC = () => {
   return (
     <>
       <AppBar />
-      <Container component="main" maxWidth={containerMaxWidth}>
-        <Box sx={{ height: 'calc(100vh - 64px)' }}>
-          <PageRouter />
-        </Box>
-      </Container>
+      <PageRouter />
     </>
   );
 };
