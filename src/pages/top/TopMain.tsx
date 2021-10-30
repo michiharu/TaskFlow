08812778c43056fde0dfb191b4ba1d152625adb5
michiharu <michi1diver@gmail.com>
2021-10-30 19:35:24 +0900

    :+1: restructure components

--- src/pages/top/TopMain.tsx
+++ src/pages/top/TopMain.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import { Card, CardActionArea, CardContent, Grid, Typography } from '@mui/material';
+import { Box, Card, CardActionArea, CardContent, Grid, Typography } from '@mui/material';
 
 import { flowPage } from '../../const';
 import { flowSelectors } from '../../store/flow';
@@ -15,19 +15,21 @@ type StateProps = {
 
 const TopMainFC: React.FC<StateProps> = ({ flows }) => {
   return (
-    <Grid container sx={{ mt: 2 }} spacing={2}>
-      {flows.map((flow) => (
-        <Grid key={flow.id} item xs={6} md={4} lg={3}>
-          <Card sx={{ minWidth: 200 }}>
-            <CardActionArea component={Link} to={flowPage.url({ id: flow.id })} disableRipple>
-              <CardContent sx={{ height: 200 }}>
-                <Typography>{flow.title}</Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <Box component="main" sx={{ p: 2 }}>
+      <Grid container spacing={2}>
+        {flows.map((flow) => (
+          <Grid key={flow.id} item xs={6} md={4} lg={3}>
+            <Card>
+              <CardActionArea component={Link} to={flowPage.url({ id: flow.id })} disableRipple>
+                <CardContent sx={{ height: 200 }}>
+                  <Typography>{flow.title}</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
