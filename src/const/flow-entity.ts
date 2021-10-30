a861910f40579b79d27b3593e19de7cfb9998b3f
michiharu <michi1diver@gmail.com>
2021-10-30 19:36:27 +0900

    :+1: change stage padding

--- src/const/flow-entity.ts
+++ src/const/flow-entity.ts
@@ -1,7 +1,7 @@
 import { FlowEntitySettings } from '../types/flow-entity';
 
 export const entitySettings: FlowEntitySettings = {
-  stagePadding: 20,
+  stagePadding: 16,
   indent: 2,
   m: 40,
   card: { width: 240, height: 120 },
